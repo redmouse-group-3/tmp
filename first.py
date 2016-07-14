d85658b616164e93f0dadf53a1ac694d804ec472
pehas <pehas.90@gmail.com>
2016-07-14 19:40:59 +0600

    Add second homework

--- first.py
+++ first.py
@@ -16,3 +16,26 @@ else:
     print("This is programm for humans")
     
 raw_input()
+
+
+
+x = int(input("Enter number from 1 to 9\n"))
+if x <=3:
+   s = str(input("Enter several numbers\n"))
+   n = int(input("How many times repeat?\n"))
+   print(s*n)
+
+elif 4 <= x <= 6:
+    m = input("Raising to the power  \n")
+    print x**m
+
+elif 7 <= x <= 9:
+    z = 0
+    while z < 10:
+        x = x + 1
+        z = z + 1
+        print(x)
+else:
+    print("Error")
+
+raw_input()
